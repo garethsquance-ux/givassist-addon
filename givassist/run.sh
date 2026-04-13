6a781c19eb1dc0be611550a370c9ad12ef300cd3
garethsquance-ux <garethsquance@gmail.com>
2026-04-13 16:16:38 +0100

    Fix Dockerfile for Pi 5 aarch64 build

--- givassist/run.sh
+++ givassist/run.sh
@@ -1,14 +1,6 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/sh
 
-# Get the HA token for API proxying
-export SUPERVISOR_TOKEN="${SUPERVISOR_TOKEN}"
-
-bashio::log.info "Starting GivAssist Wizard..."
-bashio::log.info "Access via the sidebar or http://localhost:8099"
-
-# Substitute the supervisor token into nginx config
-envsubst '${SUPERVISOR_TOKEN}' < /etc/nginx/http.d/default.conf > /tmp/nginx.conf
-mv /tmp/nginx.conf /etc/nginx/http.d/default.conf
+echo "Starting GivAssist Wizard..."
 
 # Start nginx
-exec nginx -g 'daemon off;'
+nginx -g 'daemon off;'
