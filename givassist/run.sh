bcc7ee94ecadd7c642860f824db934d5dd8e7a7d
garethsquance-ux <garethsquance@gmail.com>
2026-04-13 16:26:03 +0100

    Use s6-overlay service instead of CMD - fixes pid 1 error

--- givassist/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "=== GivAssist Wizard Starting ==="
-echo "Checking files..."
-ls -la /app/
-echo "Starting nginx on port 8099..."
-exec nginx -g 'daemon off; error_log stderr info;'
